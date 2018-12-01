efd9412c9bf7ed48ac08ccb119512f2166f6e275
James Baldwin <jbaldwin@Quantum-book.local>
2018-12-01 00:11:50 -0500

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Header extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        This is the header
+      </div>
+    )
+  }
+}
+
+export default Header
