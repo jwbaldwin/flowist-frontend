9ea6e5641f14f1897254885ce9f5aaa08f895f71
James Baldwin <jwbaldwin3@aol.com>
2019-03-02 13:43:25 -0500

    adding empty tag

--- src/components/Home/EmptyHome/EmptyHome.jsx
+++ src/components/Home/EmptyHome/EmptyHome.jsx
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
-import { Button, Icon } from 'antd';
+import { Button, Icon, Empty } from 'antd';
 import FlowModal from '../FlowModal';
 
 export class EmptyHome extends Component {
-		state = {
-			visible: false
-		};
+	state = {
+		visible: false
+	};
 
 	showModal = () => {
 		this.setState({ visible: !this.state.visible });
 	};
- // TODO USE EMPTY ANTD TAG
+	// TODO USE EMPTY ANTD TAG
 	render() {
 		return (
 			<div>
-				<FlowModal visible={this.state.visible} /> 
-				<div style={{ color: '#bbb', marginBottom: '3em' }}>
-					No active flows. Add one here!
-					<span>
-						<br />
-						<Icon type="bulb" /> Tip: use `⌘ + S`
-					</span>
-				</div>
-				<div>
+				<FlowModal visible={this.state.visible} />
+				<Empty
+					// image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+					description={
+						<div style={{ color: '#bbb', marginBottom: '3em' }}>
+							No active flows. Add one here!
+							<span>
+								<br />
+								<Icon type="bulb" /> Tip: use `⌘ + S`
+							</span>
+						</div>
+					}
+				>
 					<Button icon="thunderbolt" size="large" type="primary" onClick={this.showModal}>
-						Quick Flow!
+						Add Flow!
 					</Button>
-				</div>
+				</Empty>
 			</div>
 		);
 	}
