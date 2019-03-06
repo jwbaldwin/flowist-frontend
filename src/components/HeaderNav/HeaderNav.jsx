b44a6ad1f7d929f382098c78ff2bbe7d55112899
James Baldwin <jwbaldwin3@aol.com>
2019-03-06 20:46:05 +0000

    add in user to header

--- src/components/HeaderNav/HeaderNav.jsx
+++ src/components/HeaderNav/HeaderNav.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
-import { Layout, Menu, Icon, Button } from 'antd';
+import { Avatar, Layout, Menu, Dropdown, Icon, Button } from 'antd';
 import logo from '../../images/flowist.png';
 import './HeaderNav.css';
 
 const { Header } = Layout;
+
+const userMenu = (
+  <Menu>
+    <Menu.Item key="0">
+        <Link to="/">
+            <Icon type='user'/> Profile
+        </Link>
+    </Menu.Item>
+    <Menu.Item key="1">
+        <Link to="/">
+            <Icon type='setting'/> User Settings
+        </Link>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="2">
+        <Link to="/user">
+            <Icon type='logout'/> Log out
+        </Link>
+    </Menu.Item>
+  </Menu>
+);
 
 export class HeaderNav extends Component {
 	constructor(props) {
@@ -20,10 +41,15 @@ export class HeaderNav extends Component {
 		return (
 		    <Header style={{ background: '#fff', paddingLeft: '16px' }}>
                 <Icon
-                className="trigger"
+                className="header-trigger"
                 type={this.props.collapsed ? 'menu-unfold' : 'menu-fold'}
                 onClick={this.props.toggle}
                 />
+                <div className='header-right'>
+                    <Dropdown overlay={userMenu} placement="bottomRight">
+                        <Avatar icon="user" />
+                    </Dropdown>
+                </div>
             </Header>
 		);
 	}
