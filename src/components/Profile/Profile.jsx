c1d79db110f2fba902b0af62ff0a4ec45885e52d
James Baldwin <jwbaldwin3@aol.com>
2019-03-13 19:35:53 +0000

    statistics

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Row, Col, Card } from 'antd';
+import { Layout, Row, Col, Avatar, Icon, Statistic } from 'antd';
 
 const { Content } = Layout;
 
@@ -10,13 +10,17 @@ export class Settings extends Component {
 				<Row gutter={32}>
                     <Col span={8}>
                         <Content style={{backgroundColor: '#fff', padding: 16}}>
+                        <Avatar size={64} icon="user" />
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </Content>
                     </Col>
                     <Row>
                         <Col span={16}>
                         <Content style={{backgroundColor: '#fff', padding: 16, marginBottom: 16}}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            <Col span={12}>
+                                <Statistic title="Feedback" value={1128} prefix={<Icon type="like" />} />
+                            </Col>
+                            <Statistic title="Unmerged" value={93} suffix="/ 100" />
                         </Content>
                         </Col>
                         <Col span={16}>
