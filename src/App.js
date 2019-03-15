79e0151310dfb03cf91afb39bbc0ca53249c8d0e
James Baldwin <jwbaldwin3@aol.com>
2019-03-15 18:10:19 +0000

    protected route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AppMain from './components/AppMain';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import UserAuth from './components/UserAuth';
@@ -12,11 +12,22 @@ class App extends Component {
 			<BrowserRouter>
 				<Switch>
 					<Route exact path="/user" component={UserAuth} />
-					<Route component={AppMain} />
+					<ProtectedRoute authed={this.props.user.isAuthenticated} component={AppMain} />
 				</Switch>
 			</BrowserRouter>
 		);
 	}
+}
+
+function ProtectedRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/user', state: {from: props.location}}} />}
+    />
+  )
 }
 
 App.propTypes = {
