3668ef125dbfb4c93bea64490cb19b42c4931b51
James Baldwin <jwbaldwin3@aol.com>
2019-03-25 19:12:22 +0000

    adding more colors

--- src/common/flowItemMappers.js
+++ src/common/flowItemMappers.js
@@ -26,8 +26,9 @@ export function mapFlowStatusToBadge(flowStatus) {
 export function mapStringToColor(str) {
     const tagColorMap = {
         'java': '#5382a1',
-        'javascript': '#f0db4f',
-        'python': '#306998',
+        'javascript': '#f7df1e',
+        'js': '#f7df1e',
+        'python': '#4584b6',
         'code': '#0078d7',
         'fixme': '#f50',
         'fix': '#2ecc71',
@@ -43,16 +44,20 @@ export function mapStringToColor(str) {
         'active': '#1890ff',
         'paused': '#faad14',
         'completed': '#52c41a',
+        'test': '#84bd00',
+        'pass': '#008000',
+        'fail': '#cf000f',
+        'react': '#00d8ff'
     }
-    
+
     var string = str.replace('#', '');
 
     if (tagColorMap[string] !== undefined) {
         return tagColorMap[string];
     } else {
         var hash = 0;
-        for (var i = 0; i < str.length; i++) {
-            hash = str.charCodeAt(i) + ((hash << 5) - hash);
+        for (var i = 0; i < string.length; i++) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
         }
         var color = '#'
         for (var j = 0; j < 3; j++) {
