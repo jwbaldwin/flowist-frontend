d44466999b2c5bbad5950382bd9c6582801d3e01
James Baldwin <jwbaldwin3@aol.com>
2019-03-28 16:57:38 +0000

    margin

--- src/components/FlowHome/FlowHomeHeader/FlowHomeHeader.jsx
+++ src/components/FlowHome/FlowHomeHeader/FlowHomeHeader.jsx
@@ -14,7 +14,7 @@ export class FlowHomeHeader extends Component {
                 className="flow-home-header"
                 style={{ padding: 24 }}
             >
-				<div style={{marginBottom: '1em', fontWeight: '800'}}>You were <Text code>{this.props.activity}</Text> around <Text code>{<TimeAgo date={this.props.created}/>}</Text></div>
+				<div style={{marginBottom: '2em', fontWeight: '800'}}>You were <Text code>{this.props.activity}</Text> around <Text code>{<TimeAgo date={this.props.created}/>}</Text></div>
                 <Divider style={{ color: '#ccc' }}>quick actions</Divider>
                 <Row type="flex" justify="space-around" align="middle">
                     <Button className='header-btn' size='large'>
